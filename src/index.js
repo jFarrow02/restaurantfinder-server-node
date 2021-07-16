ede9be7dd6c9a621e2829bc91612a36794f8a057
jFarrow02 <jack.dempsey.farrow@gmail.com>
2021-07-15 22:25:48 -0400

    adds cors support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
+
 const RestaurantService = require('./services/restaurant-service/restaurant-service');
 const RestaurantsRouter = require('./routers/restaurants')(RestaurantService);
 const CuisineTypeService = require('./services/cuisine-type/cuisine-type-service');
 const CuisineTypeRouter = require('./routers/cuisine-types')(CuisineTypeService);
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+}
+
+server.use(cors(corsOptions));
 server.use('/restaurants', RestaurantsRouter);
 server.use('/cuisine-type', CuisineTypeRouter);
 
